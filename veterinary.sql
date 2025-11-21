e696ae6f2c1b2de46a52d900ee1ab01f933929c4
reth-ink <rethjerrod18@gmail.com>
2025-11-21 19:00:49 +0800

    feat/create-table-appointments

--- veterinary.sql
+++ veterinary.sql
@@ -21,3 +21,11 @@ CREATE TABLE animals(
     FOREIGN KEY (ownerid)
 REFERENCES owners(ownerid)
 );
+
+CREATE TABLE appointments (
+    appointid INT PRIMARY KEY,
+    animalid INT,
+    appointdate DATE,
+    reason VARCHAR(255),
+    FOREIGN KEY (animalid) REFERENCES animals(animalid)
+);
